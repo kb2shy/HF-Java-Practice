1447615e8b8c1e17e903af610fe22e00e2fbf8c6
Chhaian Pin <chhaianpin@hotmail.com>
2019-09-12 22:57:58 -0700

    cd

---
+++ Chapter9/Hippo.java
@@ -0,0 +1,8 @@
+/**
+ * Hippo
+ */
+public class Hippo extends Animal {
+  public Hippo(String name) {
+    super(name);
+  }
+}
---
+++ Chapter9/TestHippo.java
@@ -0,0 +1,11 @@
+/**
+ * TestHippo
+ */
+public class TestHippo {
+
+  public static void main(String[] args) {
+    // System.out.println("Starting...");
+    Hippo h = new Hippo("Buffy");
+    System.out.println(h.getName());
+  }
+}
