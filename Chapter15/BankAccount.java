ba47da919e81af96eb81335f1af5dae0ebed53dc
Chhaian Pin <chhaianpin@hotmail.com>
2019-09-14 12:30:28 -0700

    chapter 15 example codes completed

---
+++ Chapter15/BankAccount.java
@@ -0,0 +1,15 @@
+/**
+ * BankAccount
+ */
+public class BankAccount {
+
+  private int balance = 100;
+
+  public int getBalance() {
+    return balance;
+  }
+
+  public void withdraw(int amount) {
+    balance -= amount;
+  }
+}
---
+++ Chapter15/TestSync.java
@@ -0,0 +1,18 @@
+/**
+ * TestSync
+ */
+public class TestSync implements Runnable {
+
+  private int balance;
+  public void run() {
+    for (int i = 0; i < 10; i++) {
+      increment();
+      System.out.println("balance is " + balance);
+    }
+  }
+
+  public synchronized void increment(){
+    int i = balance;
+    balance = i + 1;
+  }
+}
